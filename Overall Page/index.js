e9040749817b3a3714600485916447bee128cb42
Ethan Carpenter <ethancarpenter19@ctkcc.co.uk>
2023-11-07 07:38:22 +0000

    go and from different pages

---
+++ Overall Page/index.js
@@ -0,0 +1,20 @@
+const navbar={
+    logo:document.getElementById("logo"),
+    apps:document.getElementById("apps"),
+    op2:document.getElementById("option2"),
+    op3:document.getElementById("option3"),
+    account:document.getElementById("account")
+};
+const links={
+    logo:"",
+    apps:"./gamePage/gamePage.html",
+    op2:"",
+    op3:"",
+    account:""
+};
+navbar.apps.addEventListener("click", button => {
+    frame=document.getElementById("frame");
+    console.log(frame);
+    frame.src=links.apps;
+    
+});
